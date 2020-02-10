71f72a9647f4391a8ddbeeb5fab7b20def7e9175
Dimon Mobile <mobik.dimka@gmail.com>
2020-02-10 14:20:16 +0300

    First lab completed

---
+++ 1/1/WebForm1.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("Page_Load");
+        }
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            Console.WriteLine("Page_Init");
+        }
+
+        protected void Page_Prerender(object sender, EventArgs e)
+        {
+            Console.WriteLine("Page_PRerender");
+        }
+
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            Console.WriteLine("Page_Unload");
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            ViewState["test"] = TextBox1.Text;
+            Label1.Text = ViewState["test"].ToString();
+        }
+
+        protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("CheckedChanged");
+        }
+    }
+}
