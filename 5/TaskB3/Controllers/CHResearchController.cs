23a15b6b2b51c2fceb3a5efcda5b8eb680354b98
D.Mobile <mobik.dimka@gmail.com>
2020-05-17 18:09:48 +0300

    Lab 5 completed

---
+++ 5/TaskB3/Controllers/CHResearchController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace TaskB3.Controllers
+{
+    public class CHResearchController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [OutputCache(Duration = 5)]
+        public ActionResult AD()
+        {
+            return Content($"AD {DateTime.Now.ToLocalTime()}");
+        }
+
+        [HttpPost]
+        [OutputCache(Duration = 7)]
+        public ActionResult AP(int x, int y)
+        {
+            return Content($"AP x={x}, y={y}");
+        }
+
+        [HttpGet]
+        public ActionResult AP()
+        {
+            return Content("Use POSTMAN and POST method to access this link");
+        }
+    }
+}
---
+++ 5/TaskB3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TaskB3.MvcApplication" Language="C#" %>
